217aca0502f4fccf75d45ca30eef1e70d664cc10
Lucas Pereira <lucaslpa12345@gmail.com>
2022-05-29 14:26:12 -0300

    add middleware cors

---
+++ config/middleware.js
@@ -0,0 +1,10 @@
+module.exports = {
+    //...
+    settings: {
+      cors: {
+        enabled: true, 
+        // headers: '*', 
+        origin: ["https://blog-flax-sigma.vercel.app"],
+      },
+    },
+  };
